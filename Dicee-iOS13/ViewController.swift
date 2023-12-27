d7c64be5f3ea95a4b22e5ea0a5bcb07f5bc8791c
Victor Novais <victor96novais@gmail.com>
2023-12-27 12:11:06 -0300

    feat/resize-constraints

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [
+             UIImage(named: "DiceOne"),
+             UIImage(named: "DiceTwo"),
+             UIImage(named: "DiceThree"),
+             UIImage(named: "DiceFour"),
+             UIImage(named: "DiceFive"),
+             UIImage(named: "DiceSix")
+        ]
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
     }
-
-
 }
-
